8c687729554a0131e2f2dba29c50b872c314c189
liaohui5 <liaohui5@foxmail.com>
2022-09-22 13:26:36 +0800

    修改了pinia的createstore

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createPinia } from "pinia";
 import { createApp } from "vue";
 import router from "@/router";
+import store from "@/store";
 import App from "@/App";
 
 const app = createApp(App);
 
 app.use(router);
-app.use(createPinia());
+app.use(store);
 
 app.mount("#app");
